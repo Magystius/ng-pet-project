716f033805e28d26f1a2a75a712eed6983044435
Tim Dekarz <tim.dekarz@gmx.net>
2018-01-06 23:09:07 +0100

    adding bootstrap & font-awesome

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app/app.module';
 import {environment} from './environments/environment';
 
+import fontawesome from '@fortawesome/fontawesome';
+import solid from '@fortawesome/fontawesome-free-solid';
+import brands from '@fortawesome/fontawesome-free-brands';
+
+fontawesome.library.add(solid);
+fontawesome.library.add(brands);
+
 if (environment.production) {
   enableProdMode();
 }
