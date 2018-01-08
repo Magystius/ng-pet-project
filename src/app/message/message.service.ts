15593ce142bde2c5faff1d1a15d9dddcc111a054
Tim Dekarz <tim.dekarz@gmx.net>
2018-01-08 21:47:54 +0100

    add debug log output for message service

--- src/app/message/message.service.ts
+++ src/app/message/message.service.ts
@@ -7,14 +7,17 @@ export class MessageService {
   constructor(private notification: MatSnackBar) { }
 
   public info(message: string) {
+    console.log('INFO: ' + message);
     this.notification.open(message, '', { duration: 2000, panelClass: 'notification-info', horizontalPosition: 'end'});
   }
 
   public success(message: string) {
+    console.log('SUCCESS: ' + message);
     this.notification.open(message, '', { duration: 2000, panelClass: 'notification-success', horizontalPosition: 'end'});
   }
 
   public error(message: string) {
+    console.log('ERROR: ' + message);
     this.notification.open(message, '', { duration: 2000, panelClass: 'notification-error', horizontalPosition: 'end'});
   }
 }
